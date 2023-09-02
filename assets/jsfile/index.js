3987a3d78b615a4f880a362ca40ab097f4137c61
khushi303 <maankhushi303@gmail.com>
2023-09-02 16:56:24 +0530

    first commit

---
+++ assets/jsfile/index.js
@@ -0,0 +1,14 @@
+function navfun() {
+    document.getElementById("nav-icon").classList.toggle("end-0");
+    document.body.classList.toggle("overflow-hidden")
+    document.getElementById("menu-icon").classList.toggle("cross");
+}
+
+setTimeout(() => {
+    console.log("Delayed for 3 second.");
+
+    document.getElementById("preloder").classList.add("opacity-0");
+    document.getElementById("preloder").classList.add("pointer_event_none");
+
+    document.body.classList.remove("overflow-hidden")
+}, 3000);
